a8339ef42854f14fdb6eef8f778836f55aadd3e9
devbhagat2041 <133623148+devbhagat2041@users.noreply.github.com>
2024-04-27 10:44:17 +0530

    Add files via upload

---
+++ about/index.php
@@ -0,0 +1,177 @@
+<?php
+
+session_start();
+
+include "../include/connection.php";
+?>
+
+<html>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<!-- Mobile Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<!-- Site Metas -->
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+
+<title>Watcher</title>
+
+<!-- slider stylesheet -->
+<link rel="stylesheet" type="text/css" href="../include/css/owl.carousel.min.css" />
+<link rel="stylesheet" href="../include/css/userlinks.css" />
+<link rel="stylesheet" href="../include/css/site_title.css" />
+
+<!-- bootstrap core css -->
+<link rel="stylesheet" type="text/css" href="../include/css/bootstrap.css" />
+
+<!-- fonts style -->
+<link href="../include/css/css.css" rel="stylesheet" />
+<!-- Custom styles for this template -->
+<link href="../include/css/style.css" rel="stylesheet" />
+<!-- responsive style -->
+<link href="../include/css/responsive.css" rel="stylesheet" />
+</head>
+
+<body class="sub_page">
+  <div class="hero_area">
+    <!-- header section starts -->
+    <?php
+
+    include "../include/header.php";
+
+    ?>
+    <!-- end header section -->
+
+  </div>
+  <div class="bg">
+
+    <!-- about section -->
+    <section class="about_section layout_padding">
+      <div class="container-fluid">
+
+        <div class="row">
+          <div class="col-md-5 offset-md-2">
+            <div class="about_detail-box">
+              <h3 class="custom_heading ">
+                About Us
+              </h3>
+              <p class="">
+                The Idea <br><br>
+                The idea for the site came about because people were often asking for advice about which watch they should buy. The more this happened — and it happened a lot — the more we realized that searching for a watch online is a minefield of information.
+
+                The individual brand websites are great, but there are hundreds of watch brands in India alone, so checking out each page individually would take you weeks, and that’s if you know the names of all the brands.
+
+                There is nowhere you can really search for all the new watches on the planet, compare models, check prices, and discover new brands all in the same place. That is, until now!
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="about_img-box">
+              <img src="../include/image/about.png" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <br><br>
+        <h3 class="custom_heading" style="text-align: center;">
+            Founders
+        </h3>
+        <br><br>
+        
+
+        <div class="row">
+          <div class="col-md-4 offset-md-2">
+            <div class="about_img-box">
+              <img src="../include/image/shrusti.png" alt="" />
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="about_detail-box">
+              <h3 class="custom_heading ">
+                Shrusti Patel
+              </h3>
+              <p class="">
+                Founder & CEO<br><br>
+                The idea for the site came about because people were often asking for advice about which watch they should buy. The more this happened — and it happened a lot — the more we realized that searching for a watch online is a minefield of information.
+
+                The individual brand websites are great, but there are hundreds of watch brands in India alone, so checking out each page individually would take you weeks, and that’s if you know the names of all the brands.
+
+                There is nowhere you can really search for all the new watches on the planet, compare models, check prices, and discover new brands all in the same place. That is, until now!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4 offset-md-2">
+            <div class="about_img-box">
+              <img src="../include/image/dev.png" alt="" />
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="about_detail-box">
+              <h3 class="custom_heading ">
+                Dev Bhagat
+              </h3>
+              <p class="">
+                Co-Founder & CFO<br><br>
+                The idea for the site came about because people were often asking for advice about which watch they should buy. The more this happened — and it happened a lot — the more we realized that searching for a watch online is a minefield of information.
+
+                The individual brand websites are great, but there are hundreds of watch brands in India alone, so checking out each page individually would take you weeks, and that’s if you know the names of all the brands.
+
+                There is nowhere you can really search for all the new watches on the planet, compare models, check prices, and discover new brands all in the same place. That is, until now!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4 offset-md-2">
+            <div class="about_img-box">
+              <img src="../include/image/liza.png" alt="" />
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="about_detail-box">
+              <h3 class="custom_heading ">
+                Liza Patel
+              </h3>
+              <p class="">
+                Co-Founder & CTO<br><br>
+                The idea for the site came about because people were often asking for advice about which watch they should buy. The more this happened — and it happened a lot — the more we realized that searching for a watch online is a minefield of information.
+
+                The individual brand websites are great, but there are hundreds of watch brands in India alone, so checking out each page individually would take you weeks, and that’s if you know the names of all the brands.
+
+                There is nowhere you can really search for all the new watches on the planet, compare models, check prices, and discover new brands all in the same place. That is, until now!
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- end about section -->
+
+  </div>
+
+
+
+
+  <!-- footer section -->
+  <?php
+
+  include "../include/footer.php";
+
+  ?>
+  <!-- footer section -->
+  <script src="../include/js/site_title.js"></script>
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+<!-- Mirrored from html.design/demo/watcher/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 25 Jan 2022 06:56:51 GMT -->
+
+</html>
